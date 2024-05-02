684674a33de8f82f3aeaf12c3ef1c62cb63c0254
Jan Steikowski <jan.steikowski@buzzwoo.de>
2024-05-02 21:10:56 +0700

    Adjust stuff for deployment

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import logger from 'jet-logger';
 
 import EnvVars from '@src/constants/EnvVars';
 import server from './server';
+import { on } from 'events';
 
 
 // **** Run **** //
 
-const SERVER_START_MSG = ('Express server started on port: ' + 
-  EnvVars.Port.toString());
+// const SERVER_START_MSG = ('Express server started on port: ' + 
+//   EnvVars.Port.toString());
 
-server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+server.listen(3000, '0.0.0.0');
